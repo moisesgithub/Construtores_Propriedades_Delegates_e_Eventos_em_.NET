25402f0b5ea8cccc7f8ffd21127dcf56e3a182a5
moisesgithub <moises.github@outlook.com>
2021-12-17 22:29:43 -0300

    Contrutores na prática!

---
+++ Construtores/ExemploConstrutores/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Moises", "Pereira");
+            p1.Apresentar();
+        }
+    }
+}
