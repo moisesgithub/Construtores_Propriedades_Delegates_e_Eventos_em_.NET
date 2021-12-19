761b792efe01df43663eff158e7165b36bda0b76
moisesgithub <moises.github@outlook.com>
2021-12-19 15:30:34 -0300

    Contrutores Privado

--- Construtores/ExemploConstrutores/Program.cs
+++ Construtores/ExemploConstrutores/Program.cs
@@ -7,8 +7,16 @@ namespace ExemploConstrutores
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa("Moises", "Pereira");
-            p1.Apresentar();
+            Log log = Log.GetInstance();
+            
+            log.PropriedadeLog = "Teste instancia";
+
+            Log log2 = Log.GetInstance();
+            System.Console.WriteLine(log2.PropriedadeLog);
+
+            
+            //Pessoa p1 = new Pessoa("Moises", "Pereira");
+            //p1.Apresentar();
         }
     }
 }
